4a0f2738e7e7579851c6c9c6ab758e4c33ba8c5e
didilero <didileroVA1999@hotmail.fr>
2018-12-19 14:38:03 +0100

    encule de theo

--- GSB/src/vue/V_Accueil.java
+++ GSB/src/vue/V_Accueil.java
@@ -1,5 +1,6 @@
 package vue;
 import java.awt.*;
+
 import javax.swing.*;
 
 
@@ -17,14 +18,15 @@ public class V_Accueil extends JPanel {
 				+ "<h1>Bienvenue</h1>"
 				+ "</body>"
 				+ "</html>";
-		this.txt= new JLabel(message);
+		this.txt= new JLabel(message,JLabel.CENTER);
 		this.setLayout(new GridLayout(3,1));
 		ImageIcon icone=new ImageIcon(Object.class.getResource("/Image/logo.jpg"));
 		this.image=new JLabel(icone);
 		
 		this.setBackground(new Color(243,169,47));
-		this.add(this.image);
 		this.add(this.txt);
+		this.add(this.image);
+		
 	}
 	
 }
